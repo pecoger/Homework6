99f634c7e0bdf4be7ae06713ef9a76a200cd761a
Petko Gerdemski <peco20@abv.bg>
2022-03-10 02:19:30 +0200

    all tasks completed

--- src/main/java/Numbers.java
+++ src/main/java/Numbers.java
@@ -1,4 +1,4 @@
-public class PositiveAndEqualNumbers {
+public class Numbers {
 
     int a;
     int b;
@@ -28,13 +28,13 @@ public class PositiveAndEqualNumbers {
         this.c = c;
     }
 
-    public PositiveAndEqualNumbers(int a, int b, int c) {
+    public Numbers(int a, int b, int c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
-    public PositiveAndEqualNumbers() {
+    public Numbers() {
     }
 
     public void checkForPositiveNumber(){
@@ -50,6 +50,10 @@ public class PositiveAndEqualNumbers {
             System.out.printf("The number %d is positive.%n",c);
         }else {
             System.out.printf("The number %d is negative.%n",c);
+        }if (a >= 0 && b >= 0 && c >= 0){
+            System.out.println("All numbers are positive.");
+        }else {
+            System.out.println("Not all numbers are positive.");
         }
     }
 
@@ -59,5 +63,15 @@ public class PositiveAndEqualNumbers {
         }else {
             System.out.println("The numbers are not equal.");
         }
+    }
+
+    public void calculateAverage (double a, double b){
+        double average = (a + b)/2;
+        System.out.println("The average is: " + average);
+    }
+
+    public void calculateSum(double a, double b){
+        double sum = a + b;
+        System.out.println("The sum is: " + sum);
     }
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,19 +2,25 @@ public class Test {
 
     public static void main(String[] args) {
 
-        PositiveAndEqualNumbers setOfNumbers1 = new PositiveAndEqualNumbers(1, 2, 3);
+        Numbers setOfNumbers1 = new Numbers(1, 2, 3);
         setOfNumbers1.checkForPositiveNumber();
         setOfNumbers1.checkForEquality();
 
-        PositiveAndEqualNumbers setOfNumbers2 = new PositiveAndEqualNumbers();
+        Numbers setOfNumbers2 = new Numbers();
         setOfNumbers2.setA(4);
         setOfNumbers2.setB(-4);
         setOfNumbers2.setC(0);
         setOfNumbers2.checkForPositiveNumber();
         setOfNumbers2.checkForEquality();
 
-        PositiveAndEqualNumbers setOfNumbers3 = new PositiveAndEqualNumbers(9, 9, 9);
+        Numbers setOfNumbers3 = new Numbers(9, 9, 9);
         setOfNumbers3.checkForPositiveNumber();
         setOfNumbers3.checkForEquality();
+
+        Numbers average = new Numbers();
+        average.setA(7);
+        average.setB(6);
+        average.calculateAverage(average.a, average.b);
+        average.calculateSum(average.a, average.b);
     }
 }
