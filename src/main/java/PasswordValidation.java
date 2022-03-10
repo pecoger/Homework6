2db7f84447fea972899f0d984cb31ed33113e12d
Petko Gerdemski <peco20@abv.bg>
2022-03-10 17:16:39 +0200

    password validation is completed

--- src/main/java/PasswordValidation.java
+++ src/main/java/PasswordValidation.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class PasswordValidation {
 
+    public Scanner scanner = new Scanner(System.in);
+
+    private String password = scanner.nextLine();
+
+    //copied regex and removed the following modules (?=.*[A-Z])(?=\S+$) because the condition doesn't require uppercase and blank spaces.
+    static String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[@#$%^&+=]).{8,}$";
+
+    public PasswordValidation(){
+    }
+
+    public void validatePassword(){
+        if(password.matches(regex)){
+            System.out.println("The password is valid.");
+        }else {
+            System.out.println("The password is not valid.");
+        }
+    }
 }
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,19 +7,23 @@ public class Test {
         setOfNumbers1.checkForEquality();
 
         Numbers setOfNumbers2 = new Numbers();
-        setOfNumbers2.setA(4);
+        setOfNumbers2.setA(-4);
         setOfNumbers2.setB(-4);
-        setOfNumbers2.setC(0);
+        setOfNumbers2.setC(-4);
         setOfNumbers2.checkForPositiveNumber();
         setOfNumbers2.checkForEquality();
 
-        Numbers setOfNumbers3 = new Numbers(9, 9, 9);
-        setOfNumbers3.checkForPositiveNumber();
-        setOfNumbers3.checkForEquality();
+//        Numbers setOfNumbers3 = new Numbers(9, 9, 9);
+//        setOfNumbers3.checkForPositiveNumber();
+//        setOfNumbers3.checkForEquality();
 
         System.out.println(calculateAverage(6,23));
 
         System.out.println(calculateSum(calculateAverage(3, 6), calculateAverage(5, 9)));
+
+        System.out.println("Please enter password.");
+        PasswordValidation newPass = new PasswordValidation();
+        newPass.validatePassword();
         }
 
         public static double calculateAverage(double a, double b){
